0b1ea069e8fbd26c0fe1d99dc548640a6fdfa3df
matheoleger <matheo.leger@ynov.com>
2023-11-11 23:41:12 +0100

    feat(appname): Change the name of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header';
 import { Content } from "./components/Content";
